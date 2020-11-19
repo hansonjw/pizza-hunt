4061966c4b4adec190bfad30929f95b929a6dfdd
hansonjw <hansonjw@gmail.com>
2020-11-18 20:35:30 -0600

    Add validation in models and routes for pizzas and comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,11 +4,15 @@ const dateFormat = require('../utils/dateFormat');
 const PizzaSchema = new Schema(
   {
     pizzaName: {
-      type: String
+      type: String,
+      required: 'you need to provide a valid pizza name',
+      trim: true
     },
     createdBy: {
-      type: String
-    },
+      type: String,
+      required: true,
+      trim: true
+    },    
     createdAt: {
       type: Date,
       default: Date.now,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
